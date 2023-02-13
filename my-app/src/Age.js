0b7f11111b8d070c77767a95278f1ef313b8de8a
ignazioconti <ignazioconti@gmail.com>
2023-02-13 19:01:29 +0100

    svolgimento

--- my-app/src/Age.js
+++ my-app/src/Age.js
@@ -4,11 +4,7 @@ export class Age extends React.Component {
   render() {
     return (
       <div>
-        {this.props.age > 18 ? (
-          <p>Your age is , {this.props.age}</p>
-        ) : (
-          <p>You are Young</p>
-        )}
+        <p>Your age is: {this.props.age}</p>
       </div>
     );
   }
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -5,13 +5,10 @@ export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        {!!this.props.name && (
-          <p>
-            Welcome, <strong>{this.props.name}</strong>
-          </p>
+        <p>Welcome, {<strong>{this.props.name}</strong>}</p>
+        {this.props.renderAge > 18 && this.props.renderAge < 65 && (
+          <Age age={this.props.renderAge} />
         )}
-        {!this.props.name && <p>Welcome, User</p>}
-        {this.props.renderAge && <Age age={25} />}
       </div>
     );
   }
