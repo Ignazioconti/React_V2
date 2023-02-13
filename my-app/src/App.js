6479e4c67142f03d0e3991b1705cfcee05c470ae
ignazioconti <ignazioconti@gmail.com>
2023-02-13 18:49:18 +0100

    svolgimento

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
       <div>
         <HelloWord />
         <p>
-          <Welcome name="" age={50} />
+          <Welcome name="Mario" />
         </p>
       </div>
     );
