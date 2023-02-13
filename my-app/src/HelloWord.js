d56148da9181e0a7267d35bd2b27acbccaa46456
ignazioconti <ignazioconti@gmail.com>
2023-02-13 18:28:01 +0100

    svolgimento

--- my-app/src/HelloWord.js
+++ my-app/src/HelloWord.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Message } from "./Message";
 
 export class HelloWord extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello,Word</h1>
-        <p>What a beautiful day</p>
+        <Message />
       </div>
     );
   }
