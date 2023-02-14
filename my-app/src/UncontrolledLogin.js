8c1c229158a745d3865ff740f6a0268070fe3b16
ignazioconti <ignazioconti@gmail.com>
2023-02-14 17:04:03 +0100

    Svolgimento

--- my-app/src/UncontrolledLogin.js
+++ my-app/src/UncontrolledLogin.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { createRef } from "react";
 
 export class UncontrolledLogin extends React.Component {
+  _formRef = createRef();
+
   handleFormSubmit = (event) => {
     event.preventDefault();
 
@@ -11,13 +13,27 @@ export class UncontrolledLogin extends React.Component {
     console.log({ username, password, remember });
   };
 
+  handleFormPrefill = () => {
+    this._formRef.current.elements.username.value = "prova";
+    this._formRef.current.elements.password.value = "prava123";
+    this._formRef.current.elements.remember.checked = true;
+  };
+
+  handleFocusUsername = () => {
+    this._formRef.current.elements.username.autofocus = true;
+  };
+
   render() {
     return (
       <div>
         <h3>My Uncontrolled Form</h3>
 
-        <form onSubmit={this.handleFormSubmit}>
-          <input placeholder="Username..." name="username"></input>
+        <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+          <input
+            placeholder="Username..."
+            name="username"
+            autoFocus={this.handleFocusUsername}
+          ></input>
           <input
             placeholder="Password..."
             name="password"
@@ -27,6 +43,9 @@ export class UncontrolledLogin extends React.Component {
 
           <button type="submit">Login</button>
           <button type="reset">Reset</button>
+          <button type="button" onClick={this.handleFormPrefill}>
+            Prefill Form
+          </button>
         </form>
       </div>
     );
