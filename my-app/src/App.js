6af9fe51e541e668c5f56c9df97524af9b862618
ignazioconti <ignazioconti@gmail.com>
2023-02-16 14:45:40 +0100

    Context-1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,37 @@ import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Container } from "./Container";
 import { Counter } from "./Counter";
+import { DisplayLanguage } from "./DisplayLanguage";
 import { HelloWord } from "./HelloWord";
 import { InteractiveWelcome } from "./InteractiveWelcome";
+import { LanguageContext } from "./LanguageContext";
 import { Login } from "./Login";
 import { TodoList } from "./TodoList";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Welcome } from "./Welcome";
 export class App extends React.Component {
+  state = {
+    language: "en",
+  };
+
+  handleLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
   render() {
     return (
       <div>
+        <LanguageContext.Provider value={this.state.language}>
+          <select
+            value={this.state.language}
+            onChange={this.handleLanguageChange}
+          >
+            <option value="en">English</option>
+            <option value="it">Italian</option>
+          </select>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
         <HelloWord />
         <Welcome name="John" renderAge={true} />
         <Counter
