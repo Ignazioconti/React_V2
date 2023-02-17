36d2c4231e524885c5a0378ae0e6b00792ba319f
ignazioconti <ignazioconti@gmail.com>
2023-02-17 15:48:30 +0100

    UseEffect-2

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export class Counter extends React.Component {
-  state = {
-    count: this.props.initialValue ?? 0,
-  };
+export function Counter({ initialValue, incrementAmount, incrementInterval }) {
+  const [counter, setCounter] = useState(initialValue ?? 0);
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState((state) => {
-        return {
-          count: state.count + (this.props.incrementAmount ?? 1),
-        };
-      });
-    }, this.props.incrementInterval ?? 1000);
+  function CounterIncrement() {
+    setCounter((c) => c + (incrementAmount ?? 1));
   }
 
-  render() {
-    return (
-      <div>
-        <CounterDisplay count={this.state.count} />
-      </div>
-    );
-  }
+  useEffect(() => {
+    console.log("the counter is mounted");
+    const time = setInterval(() => {
+      CounterIncrement();
+    }, incrementInterval ?? 1000);
+    return () => {
+      console.log("the counter is unmounted");
+      clearInterval(time);
+    };
+  });
+  return (
+    <div>
+      <CounterDisplay count={counter} />
+    </div>
+  );
 }
+
+// export class Counter extends React.Component {
+//   state = {
+//     count: this.props.initialValue ?? 0,
+//   };
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       this.setState((state) => {
+//         return {
+//           count: state.count + (this.props.incrementAmount ?? 1),
+//         };
+//       });
+//     }, this.props.incrementInterval ?? 1000);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <CounterDisplay count={this.state.count} />
+//       </div>
+//     );
+//   }
+// }
