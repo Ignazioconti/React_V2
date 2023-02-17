e3084dfc0807686abaf2e016c270dc7c64e293a6
ignazioconti <ignazioconti@gmail.com>
2023-02-17 15:13:29 +0100

    FunctionComponent-3

--- my-app/src/Sum.js
+++ my-app/src/Sum.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-export function Sum(props) {
-  const array = props.numbers;
+export function Sum({ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9] }) {
+  const array = numbers;
   let result = 0;
 
   for (let i = 0; i < array.length; i++) {
     result += array[i];
   }
-
   return <h1>{result}</h1>;
 }
