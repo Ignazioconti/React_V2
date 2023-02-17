b7bd7ca59cceea91a750430aea93283225aca50d
ignazioconti <ignazioconti@gmail.com>
2023-02-17 15:09:33 +0100

    FunctionComponent-1

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Welcome, {<strong>{this.props.name}</strong>}</p>
-        {this.props.renderAge && <Age age={30} />}
-      </div>
-    );
-  }
+export function Welcome(props) {
+  return (
+    <div className="welcome">
+      <p>Welcome, {<strong>{props.name}</strong>}</p>
+      {props.renderAge && <Age age={23} />}
+    </div>
+  );
 }
+
+// export class Welcome extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>Welcome, {<strong>{this.props.name}</strong>}</p>
+//         {this.props.renderAge && <Age age={30} />}
+//       </div>
+//     );
+//   }
+// }
 Welcome.defaultProps = {
   name: "Ciao",
 };
