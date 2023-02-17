862d0c431e99f9af6fabd2fc0d4b468feb483221
ignazioconti <ignazioconti@gmail.com>
2023-02-17 15:06:21 +0100

    COntext-2

--- my-app/src/DisplayLanguage.js
+++ my-app/src/DisplayLanguage.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
 
+const SetLanguage = {
+  en: { CURRENT_LANGUAGE: "The language is English" },
+  it: { CURRENT_LANGUAGE: "La lingua è Italiano" },
+};
 export class DisplayLanguage extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <LanguageContext.Consumer>
+        {(language) => {
+          return <h1>{SetLanguage[language].CURRENT_LANGUAGE}</h1>;
+        }}
+      </LanguageContext.Consumer>
+    );
   }
 }
