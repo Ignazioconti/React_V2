1ff595908ee739d69845bfa16ad7068ab85d7c22
ignazioconti <ignazioconti@gmail.com>
2023-02-25 18:21:25 +0100

    CustomHook-4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
-import { Container } from "./Container";
-import { Counter } from "./Counter";
-import { DisplayLanguage } from "./DisplayLanguage";
+// import { ClickCounter } from "./ClickCounter";
+// import { ClickTracker } from "./ClickTracker";
+// import { Container } from "./Container";
+// import { Counter } from "./Counter";
+// import { DisplayLanguage } from "./DisplayLanguage";
 import { GitHubUser } from "./GitHubUser";
 import { GitHubUserList } from "./GitHubUserList";
-import { HelloWord } from "./HelloWord";
+// import { HelloWord } from "./HelloWord";
 import { HookForm } from "./HookForm";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { LanguageContext } from "./LanguageContext";
-import { Login } from "./Login";
-import { Sum } from "./Sum";
-import { TodoList } from "./TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { Welcome } from "./Welcome";
+// import { InteractiveWelcome } from "./InteractiveWelcome";
+// import { LanguageContext } from "./LanguageContext";
+// import { Login } from "./Login";
+// import { Sum } from "./Sum";
+// import { TodoList } from "./TodoList";
+// import { UncontrolledLogin } from "./UncontrolledLogin";
+// import { Welcome } from "./Welcome";
 import { HookCounter } from "./HookCounter";
 export class App extends React.Component {
   state = {
--- my-app/src/GithubUser.js
+++ my-app/src/GithubUser.js
@@ -1,7 +1,13 @@
 import { useGitHubUser } from "./useGitHubUser";
 
 export function GitHubUser({ username }) {
-  const { data } = useGitHubUser(username);
+  const { data, error, loading } = useGitHubUser(username);
 
-  return <div>{data && <h1>{data.username}</h1>}</div>;
+  return (
+    <div>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>There has been an error</h1>}
+      {data && <h1>{data.username}</h1>}
+    </div>
+  );
 }
